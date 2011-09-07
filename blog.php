ad15a58ef73625b382b718c2b4771793f43cdce8
Ben Dalziel <b.j.dalziel@gmail.com>
2011-09-07 00:29:20 -0700

    Blog cleanup, pagination

--- blog.php
+++ blog.php
@@ -4,21 +4,24 @@ require('template.php');
 require('lib/TumblrBlog.php');
 
 $getPostId = htmlspecialchars($_GET["post_id"]);
-$getPage = htmlspecialchars($_GET["page"]);
+$currentPage = htmlspecialchars($_GET["page"]);
+if (!$currentPage) {
+  $currentPage = 1;
+}
 
-$single_post = false;
+$singlePost = false;
 if ($getPostId && is_numeric($getPostId)) {
-  $single_post = true;
+  $singlePost = true;
   $getPostId = (int) $getPostId;
 }
 
 try {
   $tBlog = new TumblrBlog('bendalziel');
-  if ($single_post) {
+  if ($singlePost) {
     $posts = $tBlog->getPost($getPostId);
   }
   else {
-    $posts = $tBlog->getPosts($getPage);
+    $posts = $tBlog->getPosts($currentPage);
   }
 }
 catch (Exception $e) {
@@ -50,6 +53,50 @@ $page_content = <<<HTML
   </section>
 HTML;
 
-print render_page($page_title, 'Ben Dalziel\'s Blog', 'blog', $page_content);
+$paginationMarkup = '';
+if (!$singlePost) {
+  $pageCount = $tBlog->getPageCount();
+  $pageTabs = '';
+  for ($i = 1; $i <= $pageCount; $i++) {
+    $tabUrl = '?page=' . $i;
+    $tabClasses = array();
+    if ($i == $currentPage) {
+      $tabClasses[] = 'active';
+      $tabUrl = '#';
+    }
+    $tabClasses = implode(' ', $tabClasses);
+
+    $pageTabs .= <<<HTML
+    <li class="{$tabClasses}"><a href="{$tabUrl}">{$i}</a></li>
+HTML;
+  }
+
+  $prevUrl = '?page=' . (int) ($currentPage - 1);
+  $nextUrl = '?page=' . (int) ($currentPage + 1);
+  $prevClasses = array('prev');
+  $nextClasses = array('next');
+  if ($currentPage == 1) {
+    $prevClasses[] = 'disabled';
+    $prevUrl = '#';
+  }
+  if ($currentPage == $pageCount) {
+    $nextClasses[] = 'disabled';
+    $nextUrl = '#';
+  }
+  $prevClasses= implode(' ', $prevClasses);
+  $nextClasses= implode(' ', $nextClasses);
+
+  $paginationMarkup .= <<<HTML
+<div class="pagination">
+  <ul>
+    <li class="{$prevClasses}"><a href="{$prevUrl}">&larr; Previous</a></li>
+    {$pageTabs}
+    <li class="{$nextClasses}"><a href="{$nextUrl}">Next &rarr;</a></li>
+  </ul>
+</div>
+HTML;
+}
+
+print render_page($page_title, 'Ben Dalziel\'s Blog', 'blog', $page_content . $paginationMarkup);
 
 ?>
