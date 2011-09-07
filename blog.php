73ee55dc49a2b4f45b5b818ae1c72851cf32f8a4
Ben Dalziel <b.j.dalziel@gmail.com>
2011-09-07 12:12:06 -0700

    Blog cleanup

--- blog.php
+++ blog.php
@@ -1,130 +1,39 @@
 <?php
 
-require('template.php');
-require('lib/TumblrBlog.php');
+require_once('template.php');
+require_once('lib/TumblrBlog.php');
+require_once('lib/TumblrPost.php');
+require_once('lib/TumblrPostRenderer.php');
+require_once('lib/TumblrBlogRenderer.php');
 
-$getPostId = htmlspecialchars($_GET["post_id"]);
+$getPostId   = htmlspecialchars($_GET["post_id"]);
 $currentPage = htmlspecialchars($_GET["page"]);
 if (!$currentPage) {
   $currentPage = 1;
 }
 
-$singlePost = false;
-if ($getPostId && is_numeric($getPostId)) {
-  $singlePost = true;
-  $getPostId = strval($getPostId);
-}
-
-try {
-  $tBlog = new TumblrBlog('bendalziel');
-  if ($singlePost) {
-    $posts = $tBlog->getPost($getPostId);
-  }
-  else {
-    $posts = $tBlog->getPosts($currentPage);
-  }
-}
-catch (Exception $e) {
-}
-
-$page_title = "Ben Dalziel's Blog. <small>From new to old.</small>";
-
-$entriesMarkup = '';
-foreach ($posts as $post) {
-  $pid = $post->getId();
-  $postMarkup = (!$singlePost) ? $post->toHtml() : $post->bodyToHtml();
-
-  $tags = $post->getTags();
-  $tagsMarkup = implode(' | ' , $tags);
-
-  $url = "bendalziel.com/blog.php?post_id=$pid";
-  $url = urlencode($url);
-  $comments = '';
-  if (!$singlePost) {
-    $comments = <<<HTML
-            <iframe src="http://www.facebook.com/plugins/comments.php?href={$url}&permalink=1" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:130px; height:16px;" allowTransparency="true"></iframe>
-HTML;
-  }
-
-  $entriesMarkup .= <<<HTML
-        <li class="well">
-          <div class="{$post_classes}">
-            {$postMarkup}
-            <h5>Tags: <small>{$tagsMarkup}</small></h5>
-            {$comments}
-          </div>
-        </li>
-HTML;
-  if ($singlePost) {
-    $page_title = $post->getTitle() . ". <small><a href=\"blog.php\">Ben Dalziel's Blog &rarr;</a></small>";
-    break;
-  }
-}
+$tBlog            = new TumblrBlog('bendalziel');
+$blogRenderer     = new TumblrBlogRenderer($tBlog, 
+					   (($currentPage) ? $currentPage : 1), 
+					   (($getPostId) ? $getPostId : null));
+$pageTitle        = $blogRenderer->getPageTitle();
+$postsMarkup      = $blogRenderer->renderPosts();
+$paginationMarkup = $blogRenderer->renderPagination();
 
 $page_content = <<<HTML
   <section>
     <div class="page-header">
-      <h1>{$page_title}</h1>
+      <h1>{$pageTitle}</h1>
     </div>
     <div id="blog-container">
       <ul>
-        {$entriesMarkup}
+        {$postsMarkup}
       </ul>
     </div>
+    {$paginationMarkup}
   </section>
 HTML;
 
-$paginationMarkup = '';
-$pageCount = $tBlog->getPageCount();
-if (!$singlePost && $pageCount > 1) {
-  $pageTabs = '';
-  for ($i = 1; $i <= $pageCount; $i++) {
-    $tabUrl = '?page=' . $i;
-    $tabClasses = array();
-    if ($i == $currentPage) {
-      $tabClasses[] = 'active';
-      $tabUrl = '#';
-    }
-    $tabClasses = implode(' ', $tabClasses);
-
-    $pageTabs .= <<<HTML
-    <li class="{$tabClasses}"><a href="{$tabUrl}">{$i}</a></li>
-HTML;
-  }
-
-  $prevUrl = '?page=' . (int) ($currentPage - 1);
-  $nextUrl = '?page=' . (int) ($currentPage + 1);
-  $prevClasses = array('prev');
-  $nextClasses = array('next');
-  if ($currentPage == 1) {
-    $prevClasses[] = 'disabled';
-    $prevUrl = '#';
-  }
-  if ($currentPage == $pageCount) {
-    $nextClasses[] = 'disabled';
-    $nextUrl = '#';
-  }
-  $prevClasses= implode(' ', $prevClasses);
-  $nextClasses= implode(' ', $nextClasses);
-
-  $paginationMarkup .= <<<HTML
-<div class="pagination">
-  <ul>
-    <li class="{$prevClasses}"><a href="{$prevUrl}">&larr; Previous</a></li>
-    {$pageTabs}
-    <li class="{$nextClasses}"><a href="{$nextUrl}">Next &rarr;</a></li>
-  </ul>
-</div>
-HTML;
-}
-
-$comments = '';
-if ($singlePost) {
-  $comments = <<<HTML
-<div id="fb-root"></div><script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script><fb:comments href="bendalziel.com/blog.php?post_id={$getPostId}" num_posts="5" width="940"></fb:comments>
-HTML;
-}
-
-print render_page($page_title, strip_tags($page_title), 'blog', $page_content . $paginationMarkup . $comments);
+print render_page($pageTitle, strip_tags($pageTitle), 'blog', $page_content);
 
 ?>
