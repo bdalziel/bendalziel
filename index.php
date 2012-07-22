efdf6d2458e1858c49540c95b4404710f610c3ec
Ben Dalziel <b.j.dalziel@gmail.com>
2012-07-21 19:09:12 -0700

    Simplifying site - minimal

--- index.php
+++ index.php
@@ -1,110 +1,42 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Ben Dalziel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Ben Dalziel">
 
-require('template.php');
+    <link href='http://fonts.googleapis.com/css?family=Overlock:400,700|Lora' rel='stylesheet' type='text/css'>
 
-$title = "Ben Dalziel.com <small>A collection of my things, virtually.</small>";
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/site.css" rel="stylesheet">
+  </head>
+  <body>
 
-$page_content = <<<HTML
-
-  <section>
-
-<!--
-    <div class="row">
-      <div class="span4 column" style="text-align: center;"><img src="assets/img/ben_dalziel.jpg" width="175" height="175" alt="Ben Dalziel" title="Ben Dalziel" class="me" /></div>
-      <div class="span11 column introduction">
-          <p>Hi, I'm Ben.</p>
-          <p>I'm an engineer working in Silicon Valley.</p>
-      </div>
+<div class="container">
+  <div class="row-fluid">
+    <div class="span3 sidebar">
+      <h1>Ben Dalziel</h1>
     </div>
--->
+    <div class="span6 primary">
+     <p>Hi, I'm Ben. I build websites for <a href="http://slytrunk.com/" title="SlyTrunk.com">Sly Trunk</a>, and <a href="resume.php" title="Ben Dalziel's Visual R&#233;sum&#233;">used to</a> work for Yahoo! Sports.</p>
 
-    <div class="page-header">
-      <h1>{$title}</h1>
+      <p>When I'm not crafting beautiful things virtually, I also enjoy being creative in the real world. My current project is creating a <a href="http://sadies-ark.tumblr.com/" title="Sadie's Ark">picture of a creature</a> every week for a year.</p>
+
+      <p>I'm originally from England but moved to San Francisco in 2006. As of August 2012, I'll call San Diego home.</p>
+
+      <p>The first album I bought was the Beatles Let it Be, but my <a href="http://www.pandora.com/profile/bendalziel" title="Ben Dalziel's Pandora Profile">favorite</a> is Abbey Road.</p>
+
     </div>
+  </div>
 
-    <div class="row">
-      <div class="span-one-third column"><h3 class="well"><a href="competition.php">Running 250 miles</a></h3></div>
-      <div class="span-one-third column"><h3 class="well"><a href="#mammoth">Backpacking in Mammoth</a></h3></div>
-      <div class="span-one-third column"><h3 class="well"><a href="#oils">Oil Pastels</a></h3></div>
+  <div class="row">
+    <div class="span3 offset9 footer">
+      <p>&copy; 2012 Ben Dalziel</p>
     </div>
+  </div>
+</div>
 
-  </section>
-
-  <!--
-    Mammoth
-  -->
-  <a name="mammoth"></a>
-  <section class="photos">
-    <div class="row">
-      <div class="span16 columns">
-        <h1>Backpacking In Mammoth. <small>August 14-17, 2011.</small></h1>
-
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="span-one-third column">
-          <p>Greg and I drove up on Saturday, hitting a <a href="http://www.mojavered.com/Default.aspx">brewery</a> and casino on the way before spending Saturday night at Coldwater Creek Campground. Alex, Gary and Rex met us at the trail head early the next morning and we set out towards Duck Pass.</p>
-          <p>After lunch at Duck Lake we pushed onto Purple lake before nightfall.</p></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6107688231/" title="The Silver Divide by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6188/6107688231_51c336e008_m.jpg" width="240" height="180" alt="The Silver Divide" class="photo"></a></div></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6108252032/" title="Looking towards Franklin Lake by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6065/6108252032_0f3a4776dd_m.jpg" width="240" height="180" alt="Looking towards Franklin Lake" class="photo"></a></div></div>
-    </div>
-
-    <div class="row">
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6107698325/" title="Keeping the monster at arms length by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6203/6107698325_2f3a422a76_m.jpg" width="240" height="180" alt="Keeping the monster at arms length" class="photo"></a></div></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6108238734/" title="Night one at Purple Lake by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6197/6108238734_d6f318d182_m.jpg" width="240" height="180" alt="Night one at Purple Lake" class="photo"></a></div></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6107699395/" title="Glen Lake by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6199/6107699395_d353b4c5cf_m.jpg" width="240" height="180" alt="Glen Lake" class="photo"></a></div></div>
-    </div>
-
-    <div class="row">
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6107705241/" title="Fly fishing by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6075/6107705241_9c2053fbf6_m.jpg" width="180" height="240" alt="Fly fishing" class="photo"></a></div></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6108248478/" title="First fish caught with a fly by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6085/6108248478_3c97cdbdab_m.jpg" width="180" height="240" alt="First fish caught with a fly" class="photo"></a></div></div>
-      <div class="span-one-third column">
-         <p>Day 2: Purple Lake to Ram Lake. Fishing the creek and smaller lakes that littered the ascent. Eventually settling in at Glen Lake, we fished until nightfall before bedding down during a dramatic moon rise.</p>
-          <p>Alex and I took off up to Franklin Lake, before tackling the crest to drop into Glennette and rejoin the others for the hike back down to Purple Lake where we spent the final night.</p>
-       </div>
-    </div>
-
-    <div class="well">
-      <a href="http://www.flickr.com/photos/77926656@N00/sets/72157627581140142/map?&fLat=37.5311&fLon=-118.9414&zl=4&map_type=hyb&order_by=recent">Map of these Photos</a> | <a href="http://www.flickr.com/photos/bendalziel/sets/72157627581140142/">Set on Flickr</a> | <em>All photos taken by Greg</em>
-    </div>
-
-  </section>
-
-
-  <!--
-    Oil Pastels
-  -->
-  <a name="oils"></a>
-  <section>
-    <div class="row">
-      <div class="span-two-thirds columns">
-        <h1>Oil Pastels. <small>For Sadie.</small></h1>
-        <p><a href="http://www.sakuraofamerica.com/oilpastel-drawing">Cray-Pas Junior Artist Oil Pastels</a>, (relatively cheap, no complaints) paired with some <a href="http://www.canson-us.com/MiTeintes.asp">Canson Mi-Teintes</a> paper (8.5x11in with nice color names like <em>Bisque</em> and <em>Tobacco</em>). I got both from <a href="http://www.accentarts.com/">Accent Arts</a> in Palo Alto, a terrific independent art supply store.</p>
-      </div>
-      <div class="span-one-third column">
-        <h1>Sadie.</h1>
-        <p>Says hello.</p>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/5965396636/" title="Mr. Zebra by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6015/5965396636_ff162c0d9f_m.jpg" width="185" height="240" alt="Mr. Zebra" class="oil-p"></a></div></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6085043802/" title="Toucan by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6071/6085043802_b0da5e6cb0_m.jpg" width="240" height="184" alt="Toucan" class="oil-p"></a></div></div>
-      <div class="span-one-third column">
-        <div class="frame shadow"><a href="http://sadiedalziel.tumblr.com/post/7830973785/impromptu-photo-shoot-in-the-crib-with-new-outfit" title="Sadie"><img src="http://29.media.tumblr.com/tumblr_lom5anOd3i1qkmwhuo1_250.jpg" width="250" height="167" alt="Sadie" class="photo"></a></div>
-      </div>
-    </div>
-
-    <div class="row">
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6086979906/" title="Spider Monkey by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6074/6086979906_b3c3f44155_m.jpg" width="240" height="184" alt="Spider Monkey" class="oil-p"></a></div></div>
-      <div class="span-one-third column"><div class="frame shadow"><a href="http://www.flickr.com/photos/bendalziel/6086980950/" title="Lady Bug by Ben Dalziel, on Flickr"><img src="http://farm7.static.flickr.com/6191/6086980950_e1650a9515_m.jpg" width="240" height="184" alt="Lady Bug" class="oil-p"></a></div></div>
-    </div>
-
-  </section>
-
-HTML;
-
-print render_page($title, strip_tags($title), 'home', $page_content);
-
-?>
+  </body>
+</html>
--- resume.php
+++ resume.php
@@ -1,65 +1,38 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Ben Dalziel</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="Ben Dalziel">
 
-require('template.php');
+    <link href='http://fonts.googleapis.com/css?family=Overlock:400,700|Lora' rel='stylesheet' type='text/css'>
 
-$title = 'Ben Dalziel\'s R&#233;sum&#233;. <small>Senior Software Engineer @ Sly Trunk.</small>';
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link href="assets/css/site.css" rel="stylesheet">
+  </head>
+  <body>
 
-$page_content = <<<HTML
-  <section>
-    <div class="page-header">
-      <h1>{$title}</h1>
+<div class="container">
+  <div class="row-fluid">
+    <div class="span3 sidebar">
+      <h1>Ben Dalziel</h1>
     </div>
+    <div class="span9 primary">
 
-      <p></p>
+      <h2>Visual R&#233;sum&#233;</h2>
+      <a href="assets/img/timeline.png"><img src="assets/img/timeline.png" alt="Ben Dalziel's Visual R&#233;sum&#233;" /></a>
 
-    <div class="row">
-      <div class="span16 columns shadow">
-        <img src="assets/img/timeline.png" alt="Ben Dalziel's Visual Reume: A time line of education and professional experience" title="Ben Dalziel's Visual Resume: A time line of education and professional experience" width="900px" width="480px" />
-      </div>
     </div>
+  </div>
 
-  </section>
-
-  <section>
-
-    <h1>The Story So Far.</h1>
-
-    <div class="row">
-      <div class="span-one-third columns">
-        <h2>Chapter 1. <small>Graduate.</small></h2>
-  <p>At Durham, I worked behind the college bar and I played rugby. My proudest achievement was captaining the rugby team to a win in the final of the prestigious Floodlit Cup Trophy.</p>
-        <p>During a summer internship at IBM, I co-invented a tool for developing Aspect Oriented applications, and implemented it for my final year project.</p>
-      </div>
-
-      <div class="span-one-third columns">
-        <h2>Chapter 2. <small>Master.</small></h2>
-        <p>I developed novel software that enabled biologists to immerse themselves in their enormous data sets. Specializing in CS for the Life Sciences, I grew the research out of a remarkable diseased placenta data set in collaboration with a team at the University of California, San Francisco.</p>
-      </div>
-
-      <div class="span-one-third columns">
-        <h2>Chapter 3. <small>Professional.</small></h2>
-        <p>From the Olympics and World Cup to Major League Baseball and the NFL, I wrote code that brought the magic of sports to users of the most visited sports site in the world.</p>
-        <p>A new chapter is beginning as I transition to front end web development.</p>
-      </div>
+  <div class="row">
+    <div class="span3 offset9 footer">
+      <p>&copy; 2012 Ben Dalziel</p>
     </div>
+  </div>
+</div>
 
-    <div class="row">
-      <div class="span-one-third columns">
-        <div class="well">Java | Maple | AspectJ | Eclipse</div>
-      </div>
-      <div class="span-one-third columns">
-        <div class="well">Java | R | MySQL | XML | NetBeans</div>
-      </div>
-      <div class="span-one-third columns">
-  <div class="well">PHP | MySQL | JS | CSS | Emacs</div>
-      </div>
-    </div>
-
-
-  </section>
-
-HTML;
-
-print render_page($title, 'Ben Dalziel\'s R&#233;sum&#233;', 'resume', $page_content, "resume.php");
-
-?>
+  </body>
+</html>
